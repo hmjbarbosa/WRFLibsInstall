caa544b0f11de11f5b8ac913c111a2aec367978f
Henrique de Melo Jorge Barbosa <hbarbosa@c24-31.cm.cluster>
2025-02-27 16:01:35 -0500

    Extra requirements for compilation on Chip

--- netcdf-fortran_clean.sh
+++ netcdf-fortran_clean.sh
@@ -8,11 +8,15 @@ tar -xzvf ../Downloads/netcdf-fortran-${tag}.tar.gz
 cd netcdf-fortran-${tag}
 
 export NCDIR=${base}/netcdf_clean
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NCDIR/lib
+
+export CPPFLAGS=$CPPFLAGS" -I${NCDIR}/include "
+export LDFLAGS=$LDFLAGS" -L${NCDIR}/lib ${NCLIBS}"
 
 export LIBS=`${NCDIR}/bin/nc-config --libs`
 
 autoreconf -i -f
-./configure --prefix=${NCDIR} 
+./configure --prefix=${NCDIR} --disable-zstandard-plugin
 #\
 #	    --disable-f03-compiler-check 
 automake -a -f 
